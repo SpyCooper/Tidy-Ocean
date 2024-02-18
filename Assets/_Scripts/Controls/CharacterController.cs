c12c79b7f01e22462d63c3773b8e98015381e366
KyleChiasson <kc428921@ohio.edu>
2024-02-18 17:30:33 -0500

    Added Teleport Back

--- Assets/_Scripts/Controls/CharacterController.cs
+++ Assets/_Scripts/Controls/CharacterController.cs
@@ -7,6 +7,7 @@ public class CharacterController : Singleton<CharacterController>
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
+    [SerializeField] private Transform dock;
 
     private void Awake()
     {
@@ -22,11 +23,17 @@ public class CharacterController : Singleton<CharacterController>
         rb.MoveRotation(rb.rotation - v.x * turnSpeed * Time.deltaTime);
         //Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0);
         Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        if (Vector3.Distance(dock.position, transform.position) > 100)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = 0;
+            transform.position = dock.transform.position;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //rb.AddForceAtPosition(Vector2.down * moveSpeed / 5, collision.transform.position);
+        rb.AddForceAtPosition(Vector2.down * moveSpeed / 5, collision.transform.position);
         if(collision.gameObject.layer == LayerMask.NameToLayer("Damage"))
             TrashCollection.Instance.RemoveRandomTrash();
     }
